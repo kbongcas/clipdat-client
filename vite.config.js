d6587a3208814f0d9da64354a0b444eb978bcd85
Kevin Bongcasan <kbongcasmis@gmail.com>
2023-09-01 15:54:38 -0500

    Added netlify config

--- vite.config.js
+++ vite.config.js
@@ -5,18 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server: {
     proxy: {
-      "/clipsapi": {
-        target: "https://clipsservice.calmmeadow-d45ed0b7.eastus.azurecontainerapps.io",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/clipsapi/, "")
-      },
-      "/uploadapi": {
-        target: "https://clipuploader.calmmeadow-d45ed0b7.eastus.azurecontainerapps.io",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/uploadapi/, "")
-      }
     }
   },
   plugins: [react()],
