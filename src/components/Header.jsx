91d7315611cbb156076d4ab81b9b90cc7be7eebd
Kevin Bongcasan <kbongcasmis@gmail.com>
2023-09-27 09:33:23 -0500

    Added package version to display on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,17 +8,25 @@ import { UserProfile } from './UserProfile';
 const Header = () => {
 
     const { user, isAuthenticated, isLoading } = useAuth0();
+    // eslint-disable-next-line no-undef
+    let version = __PACKAGE_JSON_VERSION__;
 
     return (
         <header className="bg-base-100 border-b-2 border-base-300">
             <div
                 className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8"
             >
-                <div className="block text-teal-600">
-                    <img src={Logo} alt="Logo" />
+                <div className="flex">
+                    <div className="block text-teal-600">
+                        <img src={Logo} alt="Logo" />
+                    </div>
+                    <div className="ml-1 relative">
+                        <div className="stat-desc text-xs absolute bottom-0 left-0">
+                            v{version}
+                        </div>
+                    </div>
                 </div>
-
-                { isLoading == false && <div className="flex flex-1 items-center justify-end md:justify-between">
+                {isLoading == false && <div className="flex flex-1 items-center justify-end md:justify-between">
                     <nav aria-label="Global" className="hidden md:block">
                         <ul className="flex items-center gap-6 text-sm">
                         </ul>
